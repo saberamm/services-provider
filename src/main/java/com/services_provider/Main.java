e92d2168017dcccd8b7e501b88ed186580611d1d
saber <sbaerm.com@gmail.com>
2023-10-11 03:31:28 +0330

    base completed

--- src/main/java/com/services_provider/Main.java
+++ src/main/java/com/services_provider/Main.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("aa", "aa", "aa", "Ss1#222", LocalDate.of(2000, 2, 2));
+        User user = new User("aa", "aa", "aa", "yyOs1#222", LocalDate.of(2000, 2, 2));
         System.out.println(ApplicationContext.getUserService().save(user));
     }
 }
