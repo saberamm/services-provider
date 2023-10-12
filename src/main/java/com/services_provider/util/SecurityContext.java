4cfb11eca0ce2b707ca6c79758e3d94d5cb2376e
saber <sbaerm.com@gmail.com>
2023-10-12 17:48:17 +0330

    add email to SecurityContext

--- src/main/java/com/services_provider/util/SecurityContext.java
+++ src/main/java/com/services_provider/util/SecurityContext.java
@@ -5,13 +5,13 @@ public class SecurityContext {
     public static String firstname;
     public static String lastname;
     public static String username;
-    public static String studentNumber;
+    public static String email;
 
     public static void clear() {
         id = null;
         firstname = null;
         lastname = null;
         username = null;
-        studentNumber = null;
+        email = null;
     }
 }
